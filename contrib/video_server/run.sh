a680840e0fd07f2ea4f8b8197b198e8b41886ecc
Scott <scott@datamachines>
2022-05-09 16:30:05 -0400

    documentation has run instruvtions

--- contrib/video_server/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run server
-
-# Build container
-docker build -t vserve .
-
-#docker run -p 5010:5010 -t vserve  -p 5010 --fps 5 --shape 320 320
-docker run -p 5010:5010 -t vserve  -p 5010 
