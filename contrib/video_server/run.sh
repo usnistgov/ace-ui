2c76941217fdd58071d96837de1c3a037ecf64e8
Scott <scott@datamachines>
2022-05-09 16:27:12 -0400

    remove

--- contrib/video_server/run.sh
+++ contrib/video_server/run.sh
@@ -4,10 +4,5 @@
 # Build container
 docker build -t vserve .
 
-<<<<<<< HEAD
 #docker run -p 5010:5010 -t vserve  -p 5010 --fps 5 --shape 320 320
 docker run -p 5010:5010 -t vserve  -p 5010 
-=======
-#docker run --network host -p 5010:5010 -t vserve 
-docker run -p 5010:5010 -t vserve  -p 5010 --fps 5
->>>>>>> 2123f9b3242f731a337623328e3216efe887bf27
