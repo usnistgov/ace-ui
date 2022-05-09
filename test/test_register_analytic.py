e70e56464259c71b2a515ffee69453371c97b256
Annie Weng <annieweng@datamachines.io>
2022-05-09 10:43:51 -0400

    add arg and clean up  Dockefile

--- test/test_register_analytic.py
+++ test/test_register_analytic.py
@@ -1,23 +1,25 @@
 import requests
 import json
 import sys
+import argparse
+
 
 #this script will register a analytic host with the ACE core endpoint url
 
-#python3 test_register_analytic.py   $server $analytic_host_name $analytic_name 
+#python3 test_register_analytic.py  -s $server  -a $analytic_host_name[:$port]  -n $analytic_name 
+#python3 test/test_register_analytic.py -s 192.168.255.20 -a "192.168.255.10:3333" -n act_detector
 
-#ace core server
-server="192.168.255.10"
 
-#register analytic host with the ace gpu server
-#.30 nano, .20 laptop .10 gpu server
-analytic_host="192.168.255.30"
-analytic_name="test_object_detector"
-
-if len(sys.argv)>=4 :
-  server=sys.argv[1]
-  analytic_host=sys.argv[2]
-  analytic_name=sys.argv[3]
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--server', default="192.168.255.10", help='Specify an ACE server you want to register your analytic with')
+parser.add_argument('-a', '--analytic_host', default="192.168.255.30[:3000]")
+parser.add_argument('-n', '--analytic_name', default="test_object_detector")
+args = parser.parse_args()
+print(args)
+server = args.server
+analytic_host = args.analytic_host
+analytic_name = args.analytic_name
+ 
 
 
 url = "http://{}:5000/api/v1/add_analytic".format(server)
